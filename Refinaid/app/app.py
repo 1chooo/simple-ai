02e9c12ad40b10b7a4805d972fb7bfedd0842987
1chooo <hugo970217@gmail.com>
2023-08-21 19:55:35 +0800

    add: gradio env

---
+++ Refinaid/app/app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Create Date: 2023/08/21
+Author: @1chooo(Hugo ChunHo Lin)
+Version: v0.0.1
+'''
+
+import gradio as gr
+
+def start_gradio() -> None:
+    '''
+    Main function of the app.
+    '''
+    print('Hi', gr.__name__)
--- main.py
+++ main.py
@@ -4,3 +4,8 @@ Create Date: 2023/08/21
 Author: @1chooo(Hugo ChunHo Lin)
 Version: v0.0.1
 '''
+
+from Refinaid.app.app import start_gradio
+
+if __name__ == '__main__':
+    start_gradio()
