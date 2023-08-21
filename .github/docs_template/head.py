4704825516e17b43d0e2759f6702032bc80c4382
1chooo <hugo970217@gmail.com>
2023-08-21 19:39:03 +0800

    add: test env CI/CD

---
+++ .github/docs_template/head.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+'''
+Create Date: <current_date>
+Author: <author>
+Version: <next_published_version_num>
+'''
---
+++ Refinaid/Rule/rule.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Create Date: 2023/08/21
+Author: @1chooo(Hugo ChunHo Lin)
+Version: v0.0.1
+'''
+
+class Rule:
+    @staticmethod
+    def __rule__():
+        print("Our Developing Rule\n1. ...\n2. ...")
