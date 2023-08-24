a25cce4708f44d7f32667de10d89065758283200
1chooo <hugo970217@gmail.com>
2023-08-24 21:31:46 +0800

    add: rule, test: load_df

--- test/plot_and_pages_test.py
+++ test/plot_and_pages_test.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Create Date: 2023/08/24
+Author: @1chooo(Hugo ChunHo Lin)
+Version: v0.0.1
+'''
+
 import gradio as gr
 import matplotlib.pyplot as plt
 import numpy as np
