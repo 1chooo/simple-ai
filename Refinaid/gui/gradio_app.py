745563472ff432692b7b11016fc95206b25bbec1
VincentLi1216 <sunnus.tw@gmail.com>
2023-08-27 18:24:40 +0800

    k neighbor classifier

--- Refinaid/gui/gradio_app.py
+++ Refinaid/gui/gradio_app.py
@@ -19,13 +19,16 @@ dropdown_options = {
                     "datasets": ["ds1", "ds2", "ds3"],
                     "inputs": ["ip1", "ip2", "ip3"],
                     "data_scalings": ["Standard", "Min-Max"],
-                    "models": ["Decision Tree Classifier", "test", "model3"],
+                    "models": ["Decision Tree Classifier", "K Neighbor Classifier"],
                     "plots": ["plot1", "plot2", "plot3"],
                     "model_parameters":{
                                         "decision_tree_classifier": {
                                               "criterion": ["gini", "entropy", "log_loss"],
-                                              "max_features": ["auto", "sqrt", "log2"]
-                                        }
+                                              "max_features": ["auto", "sqrt", "log2"]},
+                                        "k_neighbors_classifier": {
+                                            "weights": ["uniform", "distance"],
+                                            "algorithm": ["auto", "ball_tree", "kd_tree", "brute"]}
+                                        
                     }
 
 }
@@ -44,7 +47,7 @@ model_parameters = {
 
 model_mapping = {
                 "Decision Tree Classifier": "decision_tree_classifier",
-                "test": "test"
+                "K Neighbor Classifier": "k_neighbors_classifier"
 }
 
 model_components = {}
@@ -56,20 +59,20 @@ def model_dd_change(model_dd):
 
     comp_output_list = []
     selected_model = model_mapping[model_dd]
-    print(model_dd)
-    print(selected_model)
+    # print(model_dd)
+    # print(selected_model)
 
     for key in model_components.keys():
         if key not in ["all", "model_selector"]:
             if key == selected_model:
                 for i in range(len(model_components[key])):
-                    print(f"key:{key}\ni:{i}\nitem:{model_components[key][i]}")
+                    # print(f"key:{key}\ni:{i}\nitem:{model_components[key][i]}")
                     comp_output_list.append(model_components[key][i].update(visible=True))
             else:
                 for i in range(len(model_components[key])):
-                    print(f"key:{key}\ni:{i}\nitem:{model_components[key][i]}")
+                    # print(f"key:{key}\ni:{i}\nitem:{model_components[key][i]}")
                     comp_output_list.append(model_components[key][i].update(visible=False))
-    print(comp_output_list)
+    # print(comp_output_list)
                 
 
     return *comp_output_list,
@@ -96,9 +99,18 @@ def submit_setting_btn_click(dataset:str, inputs:list, miss_value:bool, data_sca
     except:
         raise gr.Error("Invalid Data Split")
         return
+
+    data_summary_text = ""
+    for parameter in ["dataset", "inputs", "miss_value", "data_scaling", "training", "validation", "testing"]:
+        variable_value = locals()[parameter]
+        # rm \n if it is the last line
+        if parameter == "testing":
+            data_summary_text += f"{parameter}: {variable_value}"
+        else:
+            data_summary_text += f"{parameter}: {variable_value}\n"
+    
     gr.Info("Setting Updated")
-    update_list = [dataset_dd, inputs_dd]
-    return *[component.update(visible=False) for component in update_list],
+    return data_summary.update(value=data_summary_text)
 
 with gr.Blocks() as demo:
     gr.Markdown(f"{explanatory_text['header']['title']}\n{explanatory_text['header']['body']}")
@@ -130,21 +142,26 @@ with gr.Blocks() as demo:
         gr.Markdown(f"{explanatory_text['training']['title']}\n{explanatory_text['training']['body']}")
         with gr.Row():
             with gr.Column():
-                gr.Textbox(label="Data Summary")
+                data_summary = gr.Textbox(label="Data Summary", lines=7, interactive=True)
                 model_dd = gr.Dropdown(label="Select Model", choices=dropdown_options["models"], interactive=True)
-    
-                dtc_md = gr.Markdown("### Decision Tree Classifier", interactive=True)
-                dtc_criterion_dd = gr.Dropdown(label="criterion", 
-                                               choices=dropdown_options["model_parameters"]["decision_tree_classifier"]["criterion"], interactive=True)
-                dtc_max_depth_tb = gr.Textbox(label="max_depth", interactive=True)
-                dtc_min_samples_split_sldr = gr.Slider(label="min_samples_split", minimum=2, maximum=20, step=1, interactive=True)
-                dtc_min_samples_leaf_sldr = gr.Slider(label="min_samples_leaf", minimum=1, maximum=20, step=1, interactive=True)
-                dtc_max_features_dd = gr.Dropdown(label="max_features", 
-                                                  choices=dropdown_options["model_parameters"]["decision_tree_classifier"]["max_features"], interactive=True)
-                dtc_max_leaf_nodes_tb = gr.Textbox(label="max_leaf_nodes", interactive=True)
                 
+                # decision_tree_classifier
+                dtc_md = gr.Markdown("### Decision Tree Classifier", interactive=True, visible=False)
+                dtc_criterion_dd = gr.Dropdown(label="Criterion", 
+                                               choices=dropdown_options["model_parameters"]["decision_tree_classifier"]["criterion"], value="gini", interactive=True, visible=False)
+                dtc_max_depth_tb = gr.Textbox(label="Max Depth", value="None", interactive=True, visible=False)
+                dtc_min_samples_split_sldr = gr.Slider(label="Minimum Samples Split", minimum=2, maximum=20, step=1, value=2, interactive=True, visible=False)
+                dtc_min_samples_leaf_sldr = gr.Slider(label="Minimum Samples Leaf", minimum=1, maximum=20, step=1, value=1, interactive=True, visible=False)
+                dtc_max_features_dd = gr.Dropdown(label="Max Features", 
+                                                  choices=dropdown_options["model_parameters"]["decision_tree_classifier"]["max_features"], value="None", interactive=True, visible=False)
+                dtc_max_leaf_nodes_tb = gr.Textbox(label="Max Leaf Nodes", value="None", interactive=True, visible=False)
+                
+                # k_neighbors_classifier
+                knc_md = gr.Markdown("### K Neighbors Classifier", interactive=True, visible=False)
+                knc_n_nbr_sldr = gr.Slider(label="N Neighbors", value=5, interactive=True, minimum=1, maximum=20, step=1, visible=False)
+                knc_weights_dd = gr.Dropdown(label="Weights", choices=dropdown_options["model_parameters"]["k_neighbors_classifier"]["weights"], value="uniform", interactive=True, visible=False)
+                knc_althm_dd = gr.Dropdown(label="Algorithm", choices=dropdown_options["model_parameters"]["k_neighbors_classifier"]["algorithm"], value="auto", interactive=True, visible=False)
 
-                test_lr_sldr = gr.Slider(label="Learning Rate", minimum=0.0001, maximum=0.1, step=0.0001, interactive=True)
 
                 gr.Button(value="Train")
             with gr.Column():
@@ -159,18 +176,19 @@ with gr.Blocks() as demo:
         with gr.Row():
             gr.Textbox("hello")
             gr.Textbox("hello")
+
     gr.Examples(
                 [["Titanic", ["PassengerId", "Survived", "Pclass", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], True, "standard", 70, 10, 20]],
                 [dataset_dd, inputs_dd, miss_value_chkbox, data_scale_dd, train_sldr, valid_sldr, test_sldr]
     )
 
-    submit_set_btn.click(fn=submit_setting_btn_click, inputs=[dataset_dd, inputs_dd, miss_value_chkbox, data_scale_dd, train_sldr, valid_sldr, test_sldr], outputs=[dataset_dd, inputs_dd])
+    submit_set_btn.click(fn=submit_setting_btn_click, inputs=[dataset_dd, inputs_dd, miss_value_chkbox, data_scale_dd, train_sldr, valid_sldr, test_sldr], outputs=[data_summary])
 
     model_components = {
-                        "all": [dtc_md, dtc_criterion_dd, dtc_max_depth_tb, dtc_min_samples_split_sldr, dtc_min_samples_leaf_sldr, dtc_max_features_dd, dtc_max_leaf_nodes_tb, test_lr_sldr],
+                        "all": [dtc_md, dtc_criterion_dd, dtc_max_depth_tb, dtc_min_samples_split_sldr, dtc_min_samples_leaf_sldr, dtc_max_features_dd, dtc_max_leaf_nodes_tb, knc_md, knc_althm_dd, knc_n_nbr_sldr, knc_weights_dd],
                         "model_selector": [model_dd],
                         "decision_tree_classifier":[dtc_md, dtc_criterion_dd, dtc_max_depth_tb, dtc_min_samples_split_sldr, dtc_min_samples_leaf_sldr, dtc_max_features_dd, dtc_max_leaf_nodes_tb],
-                        "test": [test_lr_sldr]
+                        "k_neighbors_classifier": [knc_md, knc_althm_dd, knc_n_nbr_sldr, knc_weights_dd]
     }
     
     model_dd.change(fn=model_dd_change, inputs=model_dd, outputs=model_components["all"])
