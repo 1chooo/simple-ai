e4fd0d3cacde332d84d92877ae9a60f4e9e8f4c5
ReeveWu <reeve0319@gmail.com>
2023-08-30 12:35:17 +0800

    update select column

--- Refinaid/Action/ML_configurations.py
+++ Refinaid/Action/ML_configurations.py
@@ -22,7 +22,6 @@ class DatasetConfig:
 
     def __init__(self, dataset: str, select_col: list[str], handling_missing_value: str, scaling: Union[str, None], data_split: list[float]):
         self.dataset = dataset
-        self.select_col = select_col
         self.handling_missing_value = handling_missing_value
         self.scaling = scaling
         self.data_split = data_split
@@ -30,6 +29,7 @@ class DatasetConfig:
         self.col_label = self._get_col_label()
         self.col_remaining = self._get_col_remaining()
         self.y_col = self._get_y_col()
+        self.select_col = select_col.append(self.y_col)
 
     def _get_col_onehot(self):
         return list(set(self.object_col[self.dataset][0]) & set(self.select_col))
