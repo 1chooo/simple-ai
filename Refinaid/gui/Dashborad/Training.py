06b5ba1ef364b1e5c6694e4fcf7e6b6c7595234f
1chooo <hugo970217@gmail.com>
2023-08-31 16:34:18 +0800

    reconstruct data summary

--- Refinaid/gui/Dashborad/Training.py
+++ Refinaid/gui/Dashborad/Training.py
@@ -13,14 +13,12 @@ class TrainingComponent:
 
     data_summary = None
     model_dd = None
-    dtc_md = None
     dtc_criterion_dd = None
     dtc_max_depth_tb = None
     dtc_min_samples_split_sldr = None
     dtc_min_samples_leaf_sldr = None
     dtc_max_features_dd = None
     dtc_max_leaf_nodes_tb = None
-    knc_md = None
     knc_n_nbr_sldr = None
     knc_weights_dd = None
     knc_althm_dd = None
@@ -38,11 +36,18 @@ class TrainingComponent:
         gr.Markdown(f"{page_content.explanatory_text['training']['title']}\n{page_content.explanatory_text['training']['body']}")
         with gr.Row():
             with gr.Column():
-                self.data_summary = gr.Textbox(label="Data Summary", lines=7, interactive=True)
-                self.model_dd = gr.Dropdown(label="Select Model", choices=page_content.dropdown_options["models"], interactive=True)
+                self.data_summary = gr.DataFrame(
+                    headers=[
+                        "Parameters",
+                        "Value"
+                    ],
+                    row_count=(7, "fixed"),
+                    col_count=(2, "fixed"),
+                    interactive=False,
+                )
             with gr.Column():
+                self.model_dd = gr.Dropdown(label="Select Model", choices=page_content.dropdown_options["models"], interactive=True)
                 # decision_tree_classifier
-                self.dtc_md = gr.Markdown("### Decision Tree Classifier", interactive=True, visible=False)
                 self.dtc_criterion_dd = gr.Dropdown(label="Criterion", 
                                             choices=page_content.dropdown_options["model_parameters"]["decision_tree_classifier"]["criterion"], value="gini", interactive=True, visible=False)
                 self.dtc_max_depth_tb = gr.Textbox(label="Max Depth", value="None", interactive=True, visible=False)
@@ -53,7 +58,6 @@ class TrainingComponent:
                 self.dtc_max_leaf_nodes_tb = gr.Textbox(label="Max Leaf Nodes", value="None", interactive=True, visible=False)
                 
                 # k_neighbors_classifier
-                self.knc_md = gr.Markdown("### K Neighbors Classifier", interactive=True, visible=False)
                 self.knc_n_nbr_sldr = gr.Slider(label="N Neighbors", value=5, interactive=True, minimum=1, maximum=20, step=1, visible=False)
                 self.knc_weights_dd = gr.Dropdown(label="Weights", choices=page_content.dropdown_options["model_parameters"]["k_neighbors_classifier"]["weights"], value="uniform", interactive=True, visible=False)
                 self.knc_althm_dd = gr.Dropdown(label="Algorithm", choices=page_content.dropdown_options["model_parameters"]["k_neighbors_classifier"]["algorithm"], value="auto", interactive=True, visible=False)
@@ -74,3 +78,4 @@ class TrainingComponent:
             self.train_img1 = gr.Plot(interactive=True)
             self.train_img2 = gr.Plot(interactive=True)
             self.train_img3 = gr.Plot(interactive=True)
+            
--- Refinaid/gui/Utils.py
+++ Refinaid/gui/Utils.py
@@ -5,6 +5,7 @@ Author: @1chooo(Hugo ChunHo Lin), @ReeveWu
 Version: v0.0.1
 '''
 
+import pandas as pd
 import gradio as gr
 from Refinaid.Action.ML_configurations import DatasetConfig, DecisionTreeModelConfig, KNNModelConfig
 
@@ -23,15 +24,21 @@ def get_data_setting(dataset:str, inputs:list, miss_value:bool, data_scaling:str
     except:
         raise gr.Error("Invalid Data Split")
 
-    data_summary_text = ""
-    for parameter in ["dataset", "inputs", "miss_value", "data_scaling", "training", "validation", "testing"]:
+    preprocessing_inputs = [
+        "dataset", 
+        "inputs", 
+        "miss_value", 
+        "data_scaling", 
+        "training", 
+        "validation", 
+        "testing"
+    ]
+    data_summary_dict = {"Parameters": [], "Value": []}
+    for parameter in preprocessing_inputs:
         variable_value = locals()[parameter]
-        # rm \n if it is the last line
-        if parameter == "testing":
-            #todo add output column
-            #todo change to dataframe
-            data_summary_text += f"{parameter}: {variable_value}"
-        else:
-            data_summary_text += f"{parameter}: {variable_value}\n"
-    
-    return data_summary_text,
+        #todo add output column
+        #todo change to dataframe
+        data_summary_dict["Parameters"].append(parameter)
+        data_summary_dict["Value"].append(variable_value)
+        
+    return pd.DataFrame(data_summary_dict)
