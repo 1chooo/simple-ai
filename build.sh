f7db9028bdfbda1dc8798674bd086065ce9da83a
1chooo <hugo970217@gmail.com>
2023-09-07 16:49:50 +0800

    update: overview

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #   GitHub: github.com/1chooo
 #   Copyright (C) 2023 Hugo ChunHo Lin All rights reserved.
 
-uvicorn main:app --host 127.0.0.1 --port 5002
+uvicorn main:app --host 127.0.0.1 --port 5002 --reload
