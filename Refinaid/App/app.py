123d600906a3f900eb2d655407b5076cfef1eeee
1chooo <hugo970217@gmail.com>
2023-09-27 19:12:42 +0800

    update: Refinaid.App coding style

--- Refinaid/App/app.py
+++ Refinaid/App/app.py
@@ -12,12 +12,11 @@ from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from Refinaid.Utils.BuildPlayground import build_and_mount_playground
 from fastapi import Form, Depends, HTTPException
-import gradio as gr
 
 app = FastAPI(
     title="SIMPLE AI",
     description="Bridging the Gap with AI For Everyone",
-    version="0.1.2",
+    version="Refinaid-v0.1.4-beta",
     docs_url="/docs",
 )
 
@@ -49,73 +48,85 @@ app = build_and_mount_playground(
 @app.get("/", response_class=HTMLResponse)
 async def page_overview(request: Request):
     return templates.TemplateResponse(
-        "index.html", {"request": request}
+        "index.html", 
+        {"request": request},
     )
 
 @app.get("/project_docs", response_class=HTMLResponse)
 async def page_project_docs(request: Request):
     return templates.TemplateResponse(
-        "docs.html", {"request": request}
+        "docs.html", 
+        {"request": request},
     )
 
 @app.get("/login", response_class=HTMLResponse)
 async def page_login(request: Request, ):
     return templates.TemplateResponse(
-        f"login.html", {"request": request}
+        f"login.html", 
+        {"request": request},
     )
 
 @app.get("/setting", response_class=HTMLResponse)
 async def page_setting(request: Request, ):
     return templates.TemplateResponse(
-        f"setting.html", {"request": request}
+        f"setting.html", 
+        {"request": request},
     )
 
 @app.get("/signup", response_class=HTMLResponse)
 async def page_signup(request: Request, ):
     return templates.TemplateResponse(
-        f"signup.html", {"request": request}
+        f"signup.html", 
+        {"request": request},
     )
 
 @app.get("/about_us", response_class=HTMLResponse)
 async def page_about_us(request: Request, ):
     return templates.TemplateResponse(
-        f"about_us.html", {"request": request}
+        f"about_us.html", 
+        {"request": request},
     )
 
 @app.get("/license", response_class=HTMLResponse)
 async def page_license(request: Request, ):
     return templates.TemplateResponse(
-        f"license.html", {"request": request}
+        f"license.html", 
+        {"request": request},
     )
 
 @app.get("/teaching", response_class=HTMLResponse)
 async def page_teaching(request: Request, ):
     return templates.TemplateResponse(
-        f"teaching.html", {"request": request}
+        f"teaching.html", 
+        {"request": request},
     )
 
 @app.get("/help", response_class=HTMLResponse)
 async def page_help(request: Request, ):
     return templates.TemplateResponse(
-        f"help.html", {"request": request}
+        f"help.html", 
+        {"request": request},
     )
 
 @app.get("/settings", response_class=HTMLResponse)
 async def page_settings(request: Request, ):
     return templates.TemplateResponse(
-        f"settings.html", {"request": request}
+        f"settings.html", 
+        {"request": request},
     )
 
 @app.get("/orders", response_class=HTMLResponse)
 async def page_orders(request: Request, ):
     return templates.TemplateResponse(
-        f"orders.html", {"request": request}
+        f"orders.html", 
+        {"request": request},
     )
 
 @app.get("/playgrounds_guideline", response_class=HTMLResponse)
 async def page_playgrounds_introduction(request: Request, ):
     return templates.TemplateResponse(
-        f"playgrounds_guideline.html", {"request": request}
+        f"playgrounds_guideline.html", 
+        {"request": request},
     )
 
 @app.get('/favicon.ico', include_in_schema=False)
